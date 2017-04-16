2d9a11e6985e4451939e90ff901873e70371f07d
Ryan <ryanimpey@live.co.uk>
2017-04-16 19:47:09 +0100

    First version upload
    Get Request and shows the current status of servers. Changes color
    according to status

--- popup.js
+++ popup.js
@@ -1,6 +1,39 @@
 window.onload = function() {
-	var xmlHttp = new XMLHttpRequest();
+		var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", 'https://status.mojang.com/check', false ); // false for synchronous request
     xmlHttp.send( null );
-		return console.log(xmlHttp.responseText);
+		var returnCodeJSON = JSON.parse(xmlHttp.responseText);
+		var spanArray = [];
+		var statusArray = [];
+		var classArray = document.getElementsByClassName('status-result');
+
+		for (var i = 0; i < 5; i++) {
+			spanArray.push(Object.values(returnCodeJSON[i]));
+		}
+
+		for (var i = 0; i < spanArray.length; i++) {
+			var statusIndicator = spanArray[i][0];
+			switch (statusIndicator) {
+				case 'green':
+						statusArray.push(['Online', 'lightgreen']);
+					break;
+				case 'yellow':
+					statusArray.push(['Issues', 'orange']);
+					break;
+				case 'red':
+					statusArray.push(['Offline', 'red']);
+					break;
+				default:
+					statusArray.push(['Offline', 'red']);
+			}
+		}
+
+		for (var i = 0; i < classArray.length; i++) {
+			classArray[i].innerHTML = statusArray[i][0];
+			classArray[i].style.color = statusArray[i][1];
+		}
+
+
+		console.log(spanArray[0][0]);
+
 }
